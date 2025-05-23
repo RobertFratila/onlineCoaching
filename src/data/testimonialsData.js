ba24dc350dc1f654b2e526b7db0a06ab0208f807
Robert <robert.m.fratila@gmail.com>
2025-05-23 23:02:12 +0300

    Add feedback files

---
+++ src/data/testimonialsData.js
@@ -0,0 +1,28 @@
+import image1 from "../assets/t-image1.png";
+import image2 from "../assets/t-image2.jpg";
+import image3 from "../assets/t-image3.jpg";
+
+export const testimonialsData = [
+  {
+    planType: 'Online Coaching',
+    image: image1,
+    review:
+      "At first, I wasn’t sure how online coaching would work, but this program exceeded my expectations! Even though I train alone at the gym, my coach is always there, tracking my progress, adjusting my plan, and keeping me accountable. I’ve gained muscle and confidence like never before!",
+    name: 'Ryan Mitchell',
+    status : 'Finance Analyst'
+  },
+  {
+    planType: 'Group Coaching',
+    image: image2,
+    review: 'Training with a group pushed me to stay consistent! The structured workouts, real-time feedback, and team energy kept me motivated. I’ve never felt stronger, and having a community to share progress with made all the difference!',
+    name: 'Jessica Moore',
+    status: 'Teacher'
+  },
+  {
+    planType: 'One2One Coaching',
+    image : image3,
+    review:'I wanted a plan that fit me, and this one-on-one coaching was exactly that! My trainer customized every detail—workouts, nutrition, even recovery tips. The personal attention made a huge impact, and I’ve achieved results I never thought possible!',
+    name: 'Liam Anderson',
+    status: "Architect"
+  }
+];
