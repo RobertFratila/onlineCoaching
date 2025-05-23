13d16c163f68dfe3aef158e2fc78c8952b560857
Robert <robert.m.fratila@gmail.com>
2025-05-23 23:04:05 +0300

    Add plans files and the text for program section

---
+++ src/data/programsData.js
@@ -0,0 +1,33 @@
+import completeBody from '../assets/completeBody.png';
+import strengthMuscle from '../assets/strengthMuscle.png';
+import fatBurn from '../assets/fatBurn.png';
+import personalizedPlan from '../assets/personalizedPlan.png';
+
+export const programsData = [
+    {
+      image: completeBody,
+      heading: "Complete Body Transformation",
+      details:
+        "The ultimate 12-week program for those serious about changing their physique. Includes progressive strength training, fat-burning cardio, and a balanced, nutrient-dense meal plan to optimize performance and recovery.",
+    },
+    {
+      image: strengthMuscle,
+      heading: "Strength & Muscle Gain Program",
+      details:
+        "A specialized plan designed to maximize muscle growth and power. Focuses on heavy lifting, progressive overload, and high-protein meal planning to build lean muscle efficiently.",
+    },
+    {
+      image: fatBurn,
+      heading: "Fat Loss & Conditioning Plan",
+      details:
+        "Burn fat, tone muscles, and improve endurance with a combination of strength training, HIIT workouts, and a calorie-optimized meal plan. Designed for those looking to get lean while maintaining energy and strength.",
+    },
+    {
+      image: personalizedPlan,
+      heading: "Personalized Coaching Plan",
+      details:
+        "A fully customized program tailored to your specific goals. Whether you want to gain muscle, lose fat, or improve performance, this plan adapts to your needs with expert guidance, meal customization, and ongoing support.",
+    },
+  ];
+
+  
